908e49d298d148ee64fe1fa93f6de1c1a58d4e07
Harper Jiang <harperjiang@msn.com>
2016-12-03 15:26:54 -0600

    add node control script

--- src/main/script/node_control.py
+++ src/main/script/node_control.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import sys
 import subprocess
 
 def kill_process():
@@ -9,13 +10,29 @@ def kill_process():
     
     for line in out.split(os.linesep):
         if line.startsWith('cc'):
-            pid = line.split('\s+')[1]
+            pid = line.split()[1]
             subprocess.run(['kill', '-9', pid])
             exit()
 
 def stop_node():
-    # Use decommission
-    subprocess.run(['~/cassandra/bin/nodetool'])
+    try:
+        # Query local UUID
+        output = subprocess.check_output(['/home/cc/cassandra/bin/nodetool','info'], universal_newlines=True)
+        id = output.split(os.linesep)[0].split()[2]
+        # kill process
+        kill_process()
+        # Use removenode
+        subprocess.run(['/home/cc/cassandra/bin/nodetool','removenode',id])
+    except subprocess.CalledProcessError as e:
+        print e.output
 
 def start_node():
-    subprocess.run(['~/cassandra/bin/cassandra'])
+    subprocess.run(['/home/cc/cassandra/bin/cassandra'])
+    
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        exit()
+    if sys.argv[1] == 'start':
+        start_node()
+    if sys.argv[1] == 'stop':
+        stop_node()
