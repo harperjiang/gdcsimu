6ae5480e3e94c23b51edab77eec9f4b8021bb6d0
Harper Jiang <harperjiang@msn.com>
2016-12-03 16:18:43 -0600

    detect DN status

--- src/main/script/node_control.py
+++ src/main/script/node_control.py
@@ -5,6 +5,10 @@ import os
 import subprocess
 from time import sleep
 
+cassandra = '/home/cc/cassandra/bin/cassandra'
+nodetool = '/home/cc/cassandra/bin/nodetool'
+host = '10.140.83.2'
+
 def kill_process():
     p1 = subprocess.Popen(["ps", "aux"], stdout=subprocess.PIPE)
     p2 = subprocess.Popen(["grep", "cas"], stdin=p1.stdout, stdout=subprocess.PIPE, universal_newlines=True)
@@ -19,19 +23,24 @@ def kill_process():
 def stop_node():
     try:
         # Query local UUID
-        output = subprocess.check_output(['/home/cc/cassandra/bin/nodetool', 'info'], universal_newlines=True)
+        output = subprocess.check_output([nodetool, 'info'], universal_newlines=True)
         id = output.split(os.linesep)[0].split()[2]
         # kill process
         kill_process()
         # Wait for the info to be passed to seed
-        sleep(15)
-        # Use removenode on seed
-        subprocess.run(['/home/cc/cassandra/bin/nodetool', '-h', '10.140.83.2', 'removenode', id])
+        while True:
+            status = subprocess.check_output([nodetool, '-h', host, status], universal_newlines=True)
+            for line in status.split(os.linesep):
+                if line.contains(id) and line.startswith('DN'):
+                    # Use removenode on seed
+                    subprocess.run([nodetool, '-h', host, 'removenode', id])
+                    return
+            sleep(5)
     except subprocess.CalledProcessError as e:
         print(e.output)
 
 def start_node():
-    subprocess.run(['/home/cc/cassandra/bin/cassandra'])
+    subprocess.run([cassandra])
     
 if __name__ == "__main__":
     if len(sys.argv) < 2:
